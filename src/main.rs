f6fd0057dfbd542de7ce95692199974abd404b93
cydry3 <ysaita09@gmail.com>
2020-05-01 16:45:10 +0900

    Add `Hello world` using syscall `execv`.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate nix;
+
+use nix::unistd::execv;
+
+use std::ffi::CStr;
+
 fn main() {
-    println!("Hello, world!");
+    let args = vec!["/bin/echo\0", "Hello, world!\0"];
+
+    let argv: Vec<_> = args
+        .into_iter()
+        .map(|a| CStr::from_bytes_with_nul(a.as_bytes()))
+        .filter_map(|a| a.ok())
+        .collect();
+
+    if !(argv.len() > 0) {
+        panic!("parsing error; 0 command/argument.")
+    }
+
+    execv(argv[0], &argv[..]).expect("Error in execv.");
 }
