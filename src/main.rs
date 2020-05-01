ea0a0751fe05ec90592a40411fde8662781c9d0e
cydry3 <ysaita09@gmail.com>
2020-05-01 18:02:30 +0900

    Add signal handler.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 extern crate nix;
 
+use nix::sys::signal::{sigaction, SaFlags, SigAction, SigHandler, SigSet, Signal};
 use nix::unistd::execv;
 
 use std::ffi::CStr;
+use std::os::raw::c_int;
 
-fn main() {
+use std::env;
+use std::thread::sleep;
+use std::time::Duration;
+
+pub extern "C" fn echo_hello_world(_: c_int) {
     let args = vec!["/bin/echo\0", "Hello, world!\0"];
 
     let argv: Vec<_> = args
@@ -18,4 +24,30 @@ fn main() {
     }
 
     execv(argv[0], &argv[..]).expect("Error in execv.");
+}
+
+pub extern "C" fn print_message(_: c_int) {
+    println!("Hello, signal handler!");
+}
+
+fn main() {
+    let mut args = env::args();
+    let _command = args.next();
+    let sub_command = args.next().unwrap();
+
+    let handler = match &*sub_command {
+        "hand" => SigHandler::Handler(print_message),
+        "echo" => SigHandler::Handler(echo_hello_world),
+        _ => panic!("unexpected argument."),
+    };
+
+    let sa = SigAction::new(handler, SaFlags::empty(), SigSet::empty());
+
+    unsafe {
+        sigaction(Signal::SIGINT, &sa).expect("fail making action");
+    }
+
+    loop {
+        sleep(Duration::from_secs(1));
+    }
 }
